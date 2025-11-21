4729a36438c4d9629c887d51d9f2029e54a20a4d
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2025-11-21 09:33:12 +0000

    chore: another fix

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,4 +2,8 @@
 declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
-  }
+}
+
+declare module 'ical' {
+    export function parse(icsData: string): any;
+}
