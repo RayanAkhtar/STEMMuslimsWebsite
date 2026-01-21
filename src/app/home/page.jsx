85466dc8e1e5aa5513f920ac8da47146b7e6e412
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2026-01-21 21:33:09 +0000

    chore: update to remove events on home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -13,7 +13,6 @@ export default function HomePage() {
             <Mission />
             <Stats /> 
 
-            <Highlights />
             <SocialMedia />
             <PrayerRoomsServer />
         </div>
